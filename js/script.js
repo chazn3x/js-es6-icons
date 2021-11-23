98eba3bbea8294397da12240d1f09329d532ea5a
Giovanni <gio.a.98@gmail.com>
2021-11-23 16:28:32 +0100

    fine bonus 1

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 const icons = [
-	{name: 'cat', prefix: 'fa-', type: 'animal', family: 'fas', color: 'orange'},
-	{name: 'crow', prefix: 'fa-', type: 'animal', family: 'fas', color: 'orange'},
-	{name: 'dog', prefix: 'fa-', type: 'animal', family: 'fas', color: 'orange'},
-	{name: 'dove', prefix: 'fa-', type: 'animal', family: 'fas', color: 'orange'},
-	{name: 'dragon', prefix: 'fa-', type: 'animal', family: 'fas', color: 'orange'},
-	{name: 'horse', prefix: 'fa-', type: 'animal', family: 'fas', color: 'orange'},
-	{name: 'hippo', prefix: 'fa-', type: 'animal', family: 'fas', color: 'orange'},
-	{name: 'fish', prefix: 'fa-', type: 'animal', family: 'fas', color: 'orange'},
-	{name: 'carrot', prefix: 'fa-', type: 'vegetable', family: 'fas', color: 'green'},
-	{name: 'apple-alt', prefix: 'fa-', type: 'vegetable', family: 'fas', color: 'green'},
-	{name: 'lemon', prefix: 'fa-', type: 'vegetable', family: 'fas', color: 'green'},
-	{name: 'pepper-hot', prefix: 'fa-', type: 'vegetable', family: 'fas', color: 'green'},
-	{name: 'user-astronaut', prefix: 'fa-', type: 'user', family: 'fas', color: 'blue'},
-	{name: 'user-graduate', prefix: 'fa-', type: 'user', family: 'fas', color: 'blue'},
-	{name: 'user-ninja', prefix: 'fa-', type: 'user', family: 'fas', color: 'blue'},
-	{name: 'user-secret', prefix: 'fa-', type: 'user', family: 'fas', color: 'blue'}
+	{name: 'cat', prefix: 'fa-', type: 'animal', family: 'fas', color: color()},
+	{name: 'crow', prefix: 'fa-', type: 'animal', family: 'fas', color: color()},
+	{name: 'dog', prefix: 'fa-', type: 'animal', family: 'fas', color: color()},
+	{name: 'dove', prefix: 'fa-', type: 'animal', family: 'fas', color: color()},
+	{name: 'dragon', prefix: 'fa-', type: 'animal', family: 'fas', color: color()},
+	{name: 'horse', prefix: 'fa-', type: 'animal', family: 'fas', color: color()},
+	{name: 'hippo', prefix: 'fa-', type: 'animal', family: 'fas', color: color()},
+	{name: 'fish', prefix: 'fa-', type: 'animal', family: 'fas', color: color()},
+	{name: 'carrot', prefix: 'fa-', type: 'vegetable', family: 'fas', color: color()},
+	{name: 'apple-alt', prefix: 'fa-', type: 'vegetable', family: 'fas', color: color()},
+	{name: 'lemon', prefix: 'fa-', type: 'vegetable', family: 'fas', color: color()},
+	{name: 'pepper-hot', prefix: 'fa-', type: 'vegetable', family: 'fas', color: color()},
+	{name: 'user-astronaut', prefix: 'fa-', type: 'user', family: 'fas', color: color()},
+	{name: 'user-graduate', prefix: 'fa-', type: 'user', family: 'fas', color: color()},
+	{name: 'user-ninja', prefix: 'fa-', type: 'user', family: 'fas', color: color()},
+	{name: 'user-secret', prefix: 'fa-', type: 'user', family: 'fas', color: color()}
 ];
 const container = document.getElementById("icons");
 // dichiarazione funzione per popolare container per tipo
@@ -40,11 +40,21 @@ function card(icon) {
 	return `
 	<div class="card flex-center">
 		<div class="card-content">
-			<div class="icon ${color}">
+			<div class="icon" style="color: ${color}">
 				<i class="${iconElement}"></i>
 			</div>
 			<div class="name">${name}</div>
 		</div>
 	</div>
 	`;
+}
+// funzione per generare colori random
+function color() {
+	let color = "#";
+	const chars = "0123456789abcdef";
+	for (let i = 0; i < 6; i++) {
+		const num = Math.floor(Math.random() * chars.length);
+		color += chars[num];
+	}
+	return color;
 }
