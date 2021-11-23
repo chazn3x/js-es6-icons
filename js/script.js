0625e5bb5f495020683799e4bff339f6b8225676
Giovanni <gio.a.98@gmail.com>
2021-11-23 15:14:05 +0100

    divisione array per tipo: forEach e if in un rigo

--- js/script.js
+++ js/script.js
@@ -19,6 +19,10 @@ const icons = [
 const container = document.getElementById("icons");
 // ciclo forEach per popolare il container con tutte le icone
 icons.forEach(icon => container.innerHTML += card(icon));
+// divisione array per tipo
+const animal = [], vegetable = [], user = [];
+icons.forEach(icon => icon.type == "animal" ? animal.push(icon) : icon.type == "vegetable" ? vegetable.push(icon) : user.push(icon));
+
 // funzione per creare una card
 function card(icon) {
 	const {name, prefix, type, family, color} = icon;
