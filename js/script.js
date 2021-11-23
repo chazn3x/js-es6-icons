90ac52a5bf236577313c0cc6aa2e65de42a30692
Giovanni <gio.a.98@gmail.com>
2021-11-24 00:57:49 +0100

    semplificazione con classi e refactoring

--- js/script.js
+++ js/script.js
@@ -54,47 +54,44 @@ const icons = [
 	{name: 'hotel', prefix: 'fa-', type: 'buildings', family: 'fas'}
 ];
 
-// varibili
+// varibili ++++
+
 const container = document.getElementById("icons");
 const select = document.getElementById("icons-filter");
-// array contenente oggetti di tipi diversi
-const arrayOfObjects = [];
+
+// codice da eseguire ++++
 
 // ordinamento tipi in ordine alfabetico
 reorderTypes();
-// creazione oggetti di tipo diverso dinamicamente e inserimento in array
-createArrayObject();
 // colori random per tipo
 randomColorsToType();
-// funzione per popolare container per tipo
-const popolate = (array) => array.forEach(icon => container.innerHTML += card(icon));
-// prima popolazione container con array contenente tutte le icone
-popolate(arrayOfObjects[0].array);
+// popolazione container
+popolate();
 // popolazione del select con le opzioni dinamiche
 popolateOptions();
 // click esclusivo sulle card
 clickCards();
 // funzione per selezionare il filtro nel select
 select.addEventListener("change", function () {
-	container.innerHTML = "";
 	const value = this.value;
-	popolate(arrayOfObjects[value].array); // ripopola il container a seconda del valore (numerico) del select
-	clickCards();
+	const cards = document.querySelectorAll(".card");
+	cards.forEach(card => card.classList.contains(`${value}`) ? card.classList.add("active") : card.classList.remove("active"));
 });
-// funzione per creare una card
-function card(icon) {
-	const {name, prefix, family, color} = icon;
-	const iconElement = `${family} ${prefix}${name}`;
-	return `
-	<div class="card flex-center">
-		<div class="card-content">
-			<div class="icon" style="color: ${color}">
-				<i class="${iconElement}"></i>
-			</div>
-			<div class="name">${name}</div>
-		</div>
-	</div>
-	`;
+
+// funzioni ++++
+
+// funzione per ordinare i tipi in ordine alfabetico
+function reorderTypes() {
+	function reorder(a, b) {
+		return a.type < b.type ? -1 : 1;
+	}
+	function reorderName(a, b) {
+		if (a.type == b.type) {
+			return a.name < b.name ? -1 : 1;
+		}
+	}
+	icons.sort(reorder);
+	icons.sort(reorderName);
 }
 // funzione per assegnare colori random per tipo
 function randomColorsToType() {
@@ -120,79 +117,46 @@ function color() {
 	}
 	return color;
 }
-// funzione per creare un option dinamico
-function option(icon, num) {
-	const {type} = icon;
+// funzione per popolare container
+function popolate() {icons.forEach(icon => container.innerHTML += card(icon))}
+// funzione per creare una card
+function card(icon) {
+	const {name, prefix, family, color, type} = icon;
 	return `
-	<option value="${num}">${type}</option>
+	<div class="card flex-center all ${type} active">
+		<div class="card-content">
+			<div class="icon" style="color: ${color}">
+				<i class="${family} ${prefix}${name}"></i>
+			</div>
+			<div class="name">${name}</div>
+		</div>
+	</div>
 	`;
 }
 // funzione per popolare il select
 function popolateOptions() {
-	arrayOfObjects.forEach((icon, i) => select.innerHTML += option(icon, i));
-}
-// funzione per creare un array con oggetti di tipi diversi
-function createArrayObject() {
-	let id = 0;
-	createObject(id, "all");
-	icons.forEach(icon => arrayOfObjects[id].array.push(icon)); //creazione primo oggetto con array con tutte le icone, id:0 e tipo:"All"
-	id++; // incrementa l'id per il nuovo oggetto
 	let type = icons[0].type; // variabile per il confronto fra i tipi delle icone in icons
-	createObject(id, type); // creazione nuovo oggetto con array vuoto
+	select.innerHTML += option(type);
 	icons.forEach((icon, i) => { // controllo dei tipi di tutte le icone in icons
 		if (icon.type != type) { // se i tipi delle icone a confronto sono diversi
-			type = icon.type; // la nuova variabile di confronto è il nuovo tipo
-			id++; // incrementa l'id per il nuovo oggetto
-			createObject(id, type); // creazione nuovo oggetto con array vuoto
-			arrayOfObjects[id].array.push(icons[i]) // inserimento icona nel nuovo array
-		} else { // se i tipi delle icone a confronto sono uguali
-			arrayOfObjects[id].array.push(icons[i]) // inserimento icona nell'array precedentemente creato
+			type = icons[i].type
+			select.innerHTML += option(type);
 		}
 	});
-	reorderIcons();
 }
-// funzione per creare un oggetto con id, tipo e array dinamici
-function createObject(num, typeName) {
-	arrayOfObjects.push({
-		id: num,
-		type: typeName,
-		array: []
-	});
+// funzione per creare un option dinamico
+function option(type) {
+	return `
+	<option value="${type}">${type}</option>
+	`;
 }
 // funzione per cliccare le card
 function clickCards() {
-	const cards = document.getElementsByClassName("card");
-	for (let i = 0; i < cards.length; i++) {
-		cards[i].addEventListener("click", function () {
-			if (!(this.classList.contains("active"))) {
-				for (let i = 0; i < cards.length; i++) {
-					cards[i].classList.remove("active")
-				}
-				this.classList.add("active");
-			} else {
-				this.classList.remove("active")
-			}
-		})
-		
-	}
-}
-// funzione per ordinare i tipi in ordine alfabetico
-function reorderTypes() {
-	function reorder(a, b) {
-		if (a.type < b.type) {return -1;}
-		if (a.type > b.type) {return 1;}
-	}
-	icons.sort(reorder);
-}
-// funzione per ordinare le icone per tipo in ordine alfabetico
-function reorderIcons() {
-	function reorder(a, b) {
-		if (a.type == b.type) {
-			if (a.name < b.name) {return -1;}
-			if (a.name > b.name) {return 1;}
-		}
-	}
-	for (let i = 0; i < arrayOfObjects.length; i++) {
-		arrayOfObjects[i].array.sort(reorder);
-	}
+	const cards = document.querySelectorAll(".card");
+	cards.forEach(card => card.addEventListener("click", function () {
+		if (!(this.classList.contains("select"))) {
+			cards.forEach(card => card.classList.remove("select"));
+			this.classList.add("select");
+		} else {this.classList.remove("select")}
+	}));
 }
