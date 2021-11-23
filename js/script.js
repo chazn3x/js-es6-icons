00b0a3873d9f4b6083ccae23895a1f974aa7246e
Giovanni <gio.a.98@gmail.com>
2021-11-23 21:52:00 +0100

    codice TOTALMENTE dinamico + click esclusivo

--- js/script.js
+++ js/script.js
@@ -1,37 +1,63 @@
 const icons = [
-	{name: 'cat', prefix: 'fa-', type: 'animal', family: 'fas', color: color()},
-	{name: 'crow', prefix: 'fa-', type: 'animal', family: 'fas', color: color()},
-	{name: 'dog', prefix: 'fa-', type: 'animal', family: 'fas', color: color()},
-	{name: 'dove', prefix: 'fa-', type: 'animal', family: 'fas', color: color()},
-	{name: 'dragon', prefix: 'fa-', type: 'animal', family: 'fas', color: color()},
-	{name: 'horse', prefix: 'fa-', type: 'animal', family: 'fas', color: color()},
-	{name: 'hippo', prefix: 'fa-', type: 'animal', family: 'fas', color: color()},
-	{name: 'fish', prefix: 'fa-', type: 'animal', family: 'fas', color: color()},
-	{name: 'carrot', prefix: 'fa-', type: 'vegetable', family: 'fas', color: color()},
-	{name: 'apple-alt', prefix: 'fa-', type: 'vegetable', family: 'fas', color: color()},
-	{name: 'lemon', prefix: 'fa-', type: 'vegetable', family: 'fas', color: color()},
-	{name: 'pepper-hot', prefix: 'fa-', type: 'vegetable', family: 'fas', color: color()},
-	{name: 'user-astronaut', prefix: 'fa-', type: 'user', family: 'fas', color: color()},
-	{name: 'user-graduate', prefix: 'fa-', type: 'user', family: 'fas', color: color()},
-	{name: 'user-ninja', prefix: 'fa-', type: 'user', family: 'fas', color: color()},
-	{name: 'user-secret', prefix: 'fa-', type: 'user', family: 'fas', color: color()}
+	{name: 'cat', prefix: 'fa-', type: 'animal', family: 'fas'},
+	{name: 'crow', prefix: 'fa-', type: 'animal', family: 'fas'},
+	{name: 'dog', prefix: 'fa-', type: 'animal', family: 'fas'},
+	{name: 'dove', prefix: 'fa-', type: 'animal', family: 'fas'},
+	{name: 'dragon', prefix: 'fa-', type: 'animal', family: 'fas'},
+	{name: 'horse', prefix: 'fa-', type: 'animal', family: 'fas'},
+	{name: 'hippo', prefix: 'fa-', type: 'animal', family: 'fas'},
+	{name: 'fish', prefix: 'fa-', type: 'animal', family: 'fas'},
+	{name: 'carrot', prefix: 'fa-', type: 'vegetable', family: 'fas'},
+	{name: 'apple-alt', prefix: 'fa-', type: 'vegetable', family: 'fas'},
+	{name: 'lemon', prefix: 'fa-', type: 'vegetable', family: 'fas'},
+	{name: 'pepper-hot', prefix: 'fa-', type: 'vegetable', family: 'fas'},
+	{name: 'user-astronaut', prefix: 'fa-', type: 'user', family: 'fas'},
+	{name: 'user-graduate', prefix: 'fa-', type: 'user', family: 'fas'},
+	{name: 'user-ninja', prefix: 'fa-', type: 'user', family: 'fas'},
+	{name: 'user-secret', prefix: 'fa-', type: 'user', family: 'fas'},
+	{name: 'facebook', prefix: 'fa-', type: 'brands', family: 'fab'},
+	{name: 'instagram', prefix: 'fa-', type: 'brands', family: 'fab'},
+	{name: 'google', prefix: 'fa-', type: 'brands', family: 'fab'},
+	{name: 'linkedin', prefix: 'fa-', type: 'brands', family: 'fab'},
+	{name: 'youtube', prefix: 'fa-', type: 'brands', family: 'fab'},
+	{name: 'amazon', prefix: 'fa-', type: 'brands', family: 'fab'},
+	{name: 'smile', prefix: 'fa-', type: 'emoji', family: 'fas'},
+	{name: 'angry', prefix: 'fa-', type: 'emoji', family: 'fas'},
+	{name: 'flushed', prefix: 'fa-', type: 'emoji', family: 'fas'},
+	{name: 'grin-wink', prefix: 'fa-', type: 'emoji', family: 'fas'},
+	{name: 'laugh', prefix: 'fa-', type: 'emoji', family: 'fas'},
+	{name: 'sad-tear', prefix: 'fa-', type: 'emoji', family: 'fas'},
+	{name: 'car', prefix: 'fa-', type: 'automotive', family: 'fas'},
+	{name: 'bus', prefix: 'fa-', type: 'automotive', family: 'fas'},
+	{name: 'ambulance', prefix: 'fa-', type: 'automotive', family: 'fas'},
+	{name: 'car-side', prefix: 'fa-', type: 'automotive', family: 'fas'},
+	{name: 'truck', prefix: 'fa-', type: 'automotive', family: 'fas'}
 ];
+
+// varibili
 const container = document.getElementById("icons");
+const select = document.getElementById("icons-filter");
+// array contenente oggetti di tipi diversi
+const arrayOfObjects = [];
+
+// creazione oggetti di tipo diverso dinamicamente e inserimento in array
+createArrayObject();
+// colori random per tipo
+randomColorsToType();
 // funzione per popolare container per tipo
 const popolate = (array) => array.forEach(icon => container.innerHTML += card(icon));
-// prima popolazione array
-popolate(icons);
-// divisione array per tipo
-const animal = [], vegetable = [], user = [];
-icons.forEach(icon => icon.type == "animal" ? animal.push(icon) : icon.type == "vegetable" ? vegetable.push(icon) : user.push(icon));
+// prima popolazione container con array contenente tutte le icone
+popolate(arrayOfObjects[0].array);
+// popolazione del select con le opzioni dinamiche
+popolateOptions();
+// click esclusivo sulle card
+clickCards();
 // funzione per selezionare il filtro nel select
-const filter = document.getElementById("icons-filter");
-filter.addEventListener("change", function () {
+select.addEventListener("change", function () {
 	container.innerHTML = "";
 	const value = this.value;
-	let choose = null;
-	value == "animal" ? choose = animal : value == "vegetable" ? choose = vegetable : value == "user" ? choose = user : choose = icons;
-	popolate(choose);
+	popolate(arrayOfObjects[value].array); // ripopola il container a seconda del valore (numerico) del select
+	clickCards();
 });
 // funzione per creare una card
 function card(icon) {
@@ -48,6 +74,20 @@ function card(icon) {
 	</div>
 	`;
 }
+// funzione per assegnare colori random per tipo
+function randomColorsToType() {
+	let type = icons[0].type; // variabile per il confronto fra i tipi delle icone in icons
+	let newColor = color(); // nuovo colore random ad una variabile
+	icons.forEach((icon) => { // controllo dei tipi di tutte le icone in icons
+		if (icon.type != type) { // se i tipi delle icone a confronto sono diversi
+			type = icon.type; // la nuova variabile di confronto è il nuovo tipo
+			newColor = color(); // il nuovo colore è un nuovo colore random
+			icon.color = newColor; // il nuovo tipo avrà un nuovo colore random
+		} else {
+			icon.color = newColor; // le icone dello stesso tipo avranno lo stesso colore random
+		}
+	});
+}
 // funzione per generare colori random
 function color() {
 	let color = "#";
@@ -58,33 +98,58 @@ function color() {
 	}
 	return color;
 }
-// funzione per creare un option
-function option(icon) {
+// funzione per creare un option dinamico
+function option(icon, num) {
 	const {type} = icon;
-	let name = "";
-	switch (type) {
-		case "animal":
-			name = "Animali";
-			break;
-		case "vegetable":
-			name = "Vegetali";
-			break;
-		case "user":
-			name = "Utenti";
-	}
 	return `
-	<option value="${type}">${name}</option>
+	<option value="${num}">${type}</option>
 	`;
 }
 // funzione per popolare il select
 function popolateOptions() {
-	let item = icons[0].type;
-	filter.innerHTML += option(icons[0]);
-	icons.forEach(icon => {
-		if (icon.type != item) {
-			filter.innerHTML += option(icon);
-			item = icon.type;
+	arrayOfObjects.forEach((icon, i) => select.innerHTML += option(icon, i));
+}
+// funzione per creare un array con oggetti di tipi diversi
+function createArrayObject() {
+	let id = 0;
+	createObject(id, "all");
+	icons.forEach(icon => arrayOfObjects[id].array.push(icon)); //creazione primo oggetto con array con tutte le icone, id:0 e tipo:"All"
+	id++; // incrementa l'id per il nuovo oggetto
+	let type = icons[0].type; // variabile per il confronto fra i tipi delle icone in icons
+	createObject(id, type); // creazione nuovo oggetto con array vuoto
+	icons.forEach((icon, i) => { // controllo dei tipi di tutte le icone in icons
+		if (icon.type != type) { // se i tipi delle icone a confronto sono diversi
+			type = icon.type; // la nuova variabile di confronto è il nuovo tipo
+			id++; // incrementa l'id per il nuovo oggetto
+			createObject(id, type); // creazione nuovo oggetto con array vuoto
+			arrayOfObjects[id].array.push(icons[i]) // inserimento icona nel nuovo array
+		} else { // se i tipi delle icone a confronto sono uguali
+			arrayOfObjects[id].array.push(icons[i]) // inserimento icona nell'array precedentemente creato
 		}
 	});
 }
-popolateOptions();
+// funzione per creare un oggetto con id, tipo e array dinamici
+function createObject(num, typeName) {
+	arrayOfObjects.push({
+		id: num,
+		type: typeName,
+		array: []
+	});
+}
+// funzione per cliccare le card
+function clickCards() {
+	const cards = document.getElementsByClassName("card");
+	for (let i = 0; i < cards.length; i++) {
+		cards[i].addEventListener("click", function () {
+			if (!(this.classList.contains("active"))) {
+				for (let i = 0; i < cards.length; i++) {
+					cards[i].classList.remove("active")
+				}
+				this.classList.add("active");
+			} else {
+				this.classList.remove("active")
+			}
+		})
+		
+	}
+}
